31d4369bd33b367d20b847cb621a154eefd6b9ad
Auralcat <ashleyzt@hi2.in>
2017-12-29 14:13:32 -0200

    Replace settings.py with base.py

--- TestAPI/settings/local.py
+++ TestAPI/settings/local.py
@@ -1,6 +1,6 @@
 # settings/local.py
+# Development environment settings
 from .base import *
 
-SECRET_KEY = ''
-SECRET_KEY=qJttY$>Y8vY<~$D6^Ah]ZS2k8G>Q~F/^$KbHAnF^{%{b(Dv*!6)}}
+SECRET_KEY='qJttY$>Y8vY<~$D6^Ah]ZS2k8G>Q~F/^$KbHAnF^{%{b(Dv*!6)}}'
 DEBUG = True
--- TestAPI/settings/production.py
+++ TestAPI/settings/production.py
@@ -1,6 +1,4 @@
 # Settings to be used by the live production server.
-<<<<<<< HEAD
-
 DEBUG = True
 
 # Security settings for production, default values are False for development
@@ -11,5 +9,3 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
 X_FRAME_OPTIONS = 'DENY'
-=======
->>>>>>> 19da176a923ccba80b03f70450568c6ed58cb588
