be96125a570a7dc12c701ba4bac315be2dde6f5e
Auralcat <ashleyzt@hi2.in>
2018-01-02 16:31:02 -0200

    Create runtime.txt

--- TestAPI/settings/production.py
+++ TestAPI/settings/production.py
@@ -1,6 +1,7 @@
-from .base import *
-
 # Settings to be used by the live production server.
+from .base import *
+import os
+
 DEBUG = False
 
 # Security settings for production, default values are False for development
@@ -18,3 +19,11 @@ CSRF_COOKIE_SECURE = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_SECONDS = 1000000
 SECURE_FRAME_DENY = True
+
+ALLOWED_HOSTS = ['django-test-api.herokuapp.com']
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
